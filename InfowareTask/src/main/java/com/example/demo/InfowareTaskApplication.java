f360cab2dd62ccefbe622c156f2cc711ad35a02e
Rohit-Rathor3 <rohitsrcem2019@gmail.com>
2023-04-11 12:58:17 +0530

    Project Initialised

---
+++ InfowareTask/src/main/java/com/example/demo/InfowareTaskApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfowareTaskApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(InfowareTaskApplication.class, args);
+		System.out.println("Application Started");
+		
+	}
+
+}
